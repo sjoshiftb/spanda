00e3d5238f7f74de96adac2be98205d9172dcd44
Shreyas Joshi <shreyas@humantic.ai>
2025-08-17 01:33:32 +0200

    Updating animations

--- src/counter.js
+++ src/counter.js
@@ -1,14 +1,22 @@
 import mojs from "@mojs/core";
 
-const bouncyCircle = new mojs.Shape({
-  shape: 'circle',
-  fill: { '#F64040': '#FC46AD' },
-  radius: { 20: 80 },
-  duration: 2000,
-  isYoyo: true,
-  isShowStart: true,
-  easing: 'elastic.inout',
-  repeat: 1,
-});
+const shiftCurve = mojs.easing.path( 'M0,100 C50,100 50,100 50,50 C50,0 50,0 100,0' );
+const scaleCurveBase = mojs.easing.path( 'M0,100 C21.3776817,95.8051376 50,77.3262711 50,-700 C50,80.1708527 76.6222458,93.9449005 100,100' );
+const scaleCurve = (p) => { return 1 + scaleCurveBase(p); };
+const nScaleCurve = (p) => { return 1 - scaleCurveBase(p)/10; };
 
-bouncyCircle.play()
+const circle = new mojs.Shape({
+  shape:        'rect',
+  fill:         { '#F64040' : '#F64040', curve: scaleCurve },
+  radius:       10,
+  rx:           3,
+  x:            { [-125] : 125, easing: shiftCurve },
+  scaleX:       { 1 : 1, curve: scaleCurve },
+  scaleY:       { 1 : 1, curve: nScaleCurve },
+  origin:       { '0 50%' : '100% 50%', easing: shiftCurve },
+
+  isYoyo:         true,
+  delay:        500,
+  duration:     800,
+  repeat:       999
+}).play();
